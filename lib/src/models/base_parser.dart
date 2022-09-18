7a770655d3ab92ae3591d3033d867834f6e226fd
Ryusaki <kempcompany05@gmail.com>
2022-09-18 23:56:44 +0200

    added base provider

--- lib/src/models/base_parser.dart
+++ lib/src/models/base_parser.dart
@@ -1,6 +1,5 @@
-import 'dart:developer';
+import 'package:meta/meta.dart';
 
-import 'package:flutter/foundation.dart';
 import 'types.dart';
 
 abstract class BaseProvider {
--- lib/src/models/types.dart
+++ lib/src/models/types.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/foundation.dart';
-
 class IProviderStats {
   final String name;
   final String baseUrl;
